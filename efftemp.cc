5f7e78df75ccd3352895e0d4c39e7ee7c76c95b2
DLinthorne <DylanLinthorne@gmail.com>
2021-07-13 08:11:32 -0400

    Formating

--- efftemp.cc
+++ efftemp.cc
@@ -3,7 +3,6 @@
 // Let Pythia8:: be implicit.
 int main(int argc, char* argv[]){
 
-	
 	Pythia *pythia = new Pythia();
 
 	fastjet::JetDefinition jetDef(fastjet::genkt_algorithm, 0.5, -1);
@@ -11,19 +10,19 @@ int main(int argc, char* argv[]){
 	pythia ->readFile("ModA.dat");
 	int nEvent = pythia -> mode("Main:numberOfEvents");
 
-  Triggers *Trig = new Triggers();
+    Triggers *Trig = new Triggers();
 
-  tool -> InitializeMatching(pythia);
+    tool -> InitializeMatching(pythia);
 
-  pythia -> init();
+    pythia -> init();
 
 	for (int iEvent = 0; iEvent < nEvent; ++iEvent) {
 
-			if (!pythia -> next()) continue;
-			Trig -> eventTree(pythia, jetDef);
-		  Trig -> MultiJetTrigger();
-			Trig -> HtTrigger();
-			Trig -> EWTrigger(pythia);
+		if (!pythia -> next()) continue;
+		Trig -> eventTree(pythia, jetDef);
+		Trig -> MultiJetTrigger();
+		Trig -> HtTrigger();
+		Trig -> EWTrigger(pythia);
 	}
 
 	Trig-> TriggerEfficiencies( pythia -> info.nAccepted() );
